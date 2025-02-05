f2972dc1afa6f149554e9ca7911fa5096698a666
Vicente García <centeclan15@gmail.com>
2025-02-05 09:49:07 +0100

    introduccion completa

--- clases/introduccion.py
+++ clases/introduccion.py
@@ -1,45 +1,103 @@
-### Un texto con 10 espacios
-texto="Esto son diez espacios:"
-espacios=" "
-punto="."
-print(texto + espacios*10 + punto)
+#--------No hay que declarar variables
+print(3+2)
+n=4
+print(n)
+nota1=5
+nota2=6
+notaMedia=(nota1+nota2)/2
+print(notaMedia)
+#------Cadenas, las cadenas se puede poner entre comillas simples o dobles.----------
+'Hola mundo'
+"Hola Mundo"
+'Esta \"palabra\" va entre comillas'
+'\t indica alineaciÃ³n'
+'\n Salto de linea'
+print('Esta \"cadena\" va entre comillas')
+print('Una linea \n otra linea')
+cadena="Hola "
+print(cadena*2)
+cadena1="Hola"
+cadena2="mundo"
+print(cadena1+" "+cadena2)
+espacio=" "
+print("Esto son diez espacios:"+(espacio*10)+".")
+#Posiciones en cadenas
+palabra="Python"
+print(palabra[1])
+print(palabra[-1])
+print(palabra[0:2])
 
-letras= ["a", "b", "c", "d", "e", "f"]
+
+numeros=[1,2,3,4,5]
+print(numeros)
+
+datos=[4,"una cadena",3.14,-15,"otra cadena","1",1]
+print(datos)
+
+len(datos)
+
+datos[1]
+
+datos[len(datos)-1]
+datos[0:2]
+datos[-7:-1]
+
+print (numeros)
+numeros=[1,2,3,4,5]
+numeros
+numeros=[6,7,8,9]
+pares=[2,4,5,8,10]
+
+pares
+
+pares[2]=6
+pares
+
+letras=['a','b','c','d','e','f']
+letras
 letras[0:3]
-print(letras[0:3])
-letras[0:3]= ["A", "B", "C"]
-print(letras)
+letras[:3]
 
+letras[0:3]
+letras
+#upper(letras)
 a=[1,2,3]
 b=[4,5,6]
 c=[7,8,9]
-r=[a,b,c]
-print(r)
-print(r[1][1])
-print(r[-1][-1])
 
-###Comparar
-print(1+1==3) 
-print(4>=3)
+r=[a,b,c]
+r[1]
+r[-1]
+r[-1][-1]
+
+1+1==3
+1+1==2
+3>2
+3!=2
 a=10
 b=5
-print(a==b*2)
-print("Hola" == "Hola")
+a==b*2
+"hola"=="hola"
 
-l1=[0,1,2]
-l2=[2,3,4]
-print(len(l1)==len(l2))
+11==[0,1,2]
+12==[2,3,4]
+11==12
 
-print(True and True)
+#len(11)==len(12)
 
-c= "Hola mundo"
+False*3
+
+not True
+True and True
+False and True
+False or True
+c="Hola mundo"
+len(c)>=20 and c[0]=="H"
 len(c)>=20 or c[0]=="H"
-print(len(c)>=20 or c[0]=="H")
 
 c="Lectura"
-print(len(c)=="H" or c[0]=="H")
-
+c[0]== "H" or c[0]=="h"
 a=5
+a=a+2
+print (a)
 a+=2
-print(a)
-
